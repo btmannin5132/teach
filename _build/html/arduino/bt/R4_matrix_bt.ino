76a65b4824c5eb43251fa48d98b7a40af42d8f32
Ben Manning <67348792+btmannin5132@users.noreply.github.com>
2025-08-07 20:43:55 +0000

    Build and publish site

--- _build/html/arduino/bt/R4_matrix_bt.ino
+++ _build/html/arduino/bt/R4_matrix_bt.ino
@@ -6,7 +6,7 @@
  * I don't reccomend making a bunch of modifications to this, 
  * as you will also likely have to update the webpage that you are connecting to.
  *
- * Once the code is on the Arduino, go to the link: https://btmannin5132.github.io/personal/ or https://btmannin5132.github.io/teach/projects.html
+ * Once the code is on the Arduino, go to the link: https://btmannin5132.github.io/teach/arduino/bt/bt-app.html or https://btmannin5132.github.io/teach/projects.html
  * DO NOT connect to the Arduino using your BT settings on your device.  The app will handle all of it.
 
  * Make sure BT is enabled on your device, but DO NOT connect to the arduino outside of the app.
